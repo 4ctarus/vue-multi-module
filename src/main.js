18cfcf9db642a1d8e48cd16594f00fcb0ea3df25
4ctarus <hatim.hemaidia@gmail.com>
2021-08-20 15:05:38 +0200

    add i18n

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { router, store } from "./services";
 import { routes as financeRoutes } from "./modules/finance/services";
 import { routes as prestaRoutes } from "./modules/presta/services";
 import App from "./App.vue";
+import i18n from './i18n'
 
 Vue.config.productionTip = false;
 
@@ -12,7 +13,8 @@ addRoutes('/presta', prestaRoutes);
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  i18n,
+  render: (h) => h(App)
 }).$mount("#app");
 
 function addRoutes(prefix, routes) {
--- src/modules/finance/components/BilanTable.vue
+++ src/modules/finance/components/BilanTable.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="bilan-table">
     <h1>Bilan table</h1>
+
+    <ul id="example-2">
+      <li v-for="(row, index) in rows" :key="row">
+        {{ index }} - {{ $t(`${row}.label`) }}
+        <span v-if="$t(`${row}.type`) === 'device'">$</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// const rows = ["vente_marchandise", "cout_achat_marchandise_vendue"];
+const rows = ["vente_marchandise", "cout_achat_marchandise_vendue"];
 
-export default {};
+export default {
+  data() {
+    return {
+      rows,
+    };
+  },
+};
 </script>
 
 <style>
 </style>
+
+<i18n>
+{
+  "en": {
+    "vente_marchandise": {
+        "label": "Vente de marchandise",
+        "type": "device",
+        "editable": true
+    },
+    "cout_achat_marchandise_vendue": {
+        "label": "(Cout d'achat des marchandises vendues)",
+        "type": "device",
+        "editable": true
+    }
+  }
+}
+</i18n>
